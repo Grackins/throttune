abf019bf77cb176efdf993eb6bb4c3637ba57317
Grace Hawkins <amoomajid99@gmail.com>
2021-11-01 12:19:06 +0330

    Implement multidraw function

--- functions.py
+++ functions.py
@@ -1,10 +1,38 @@
 import matplotlib.pyplot as plt
 
-def draw(fn, setpoint=700):
+
+COLORS = 'bgmck'
+
+
+def read_file(fn):
     with open(f'data/{fn}', 'r') as f:
-        ypoints = list(map(lambda x: int(float(x)), f.read().split()))
-    xpoints = range(len(ypoints))
-    plt.plot(xpoints, ypoints, color='b')
+        return list(map(lambda x: int(float(x)), f.read().split()))
+
+
+def write_data(fn, data):
+    with open(f'data/{fn}', 'w') as f:
+        f.write('\n'.join([str(x) for x in data]))
+
+
+def draw(fn, setpoint=700):
+    multi_draw([fn], setpoint)
+
+
+def multi_draw(fns, setpoint=700):
+    longest = 0
+    for i, fn in enumerate(fns):
+        ypoints = read_file(fn)
+        xpoints = range(len(ypoints))
+        longest = max(longest, len(ypoints))
+        plt.plot(xpoints, ypoints, color=COLORS[i % len(COLORS)])
+
+    xpoints = range(longest)
     plt.plot(xpoints, [setpoint] * len(xpoints), color='r')
     plt.savefig(f'data/{fn}.png')
     plt.show()
+
+
+def apply_filter(fn, nfn, filter_func):
+    input_data = read_file(fn)
+    output_data = list(filter_func(input_data))
+    write_data(nfn, output_data)
